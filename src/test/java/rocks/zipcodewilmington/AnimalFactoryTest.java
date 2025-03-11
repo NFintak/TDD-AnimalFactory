5d8277adf498a9cb389949552876e663f79af663
Nathan <natefintak@gmail.com>
2025-03-11 17:35:01 -0400

    AnimalFactory tests added

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -2,6 +2,11 @@ package rocks.zipcodewilmington;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.Assert;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
@@ -17,40 +22,33 @@ public class AnimalFactoryTest {
     @Test
     public void test1CreateDog() {
         //given
-
+        String givenName = "Cujo";
+        Date givenBirthDate = new Date(2018/10/31);
+        Dog dog = AnimalFactory.createDog(givenName, givenBirthDate);
         //when
-
+        String retrievedName = dog.getName();
+        Date retrievedBirthDate = dog.getBirthDate();
+        Integer retrievedID = dog.getId();
         //then
-
-    }
-
-    @Test
-    public void test2CreateDog() {
-        //given
-
-        //when
-
-        //then
-
+        Assert.assertEquals(givenName, retrievedName);
+        Assert.assertEquals(givenBirthDate, retrievedBirthDate);
+        Assert.assertTrue(retrievedID != null);
     }
 
     @Test
     public void test1CreateCat() {
         //given
-
+        String givenName = "Lydia";
+        Date givenBirthDate = new Date(2024/7/11);
+        Cat cat = AnimalFactory.createCat(givenName, givenBirthDate);
         //when
-
+        String retrievedName = cat.getName();
+        Date retrievedDate = cat.getBirthDate();
+        Integer retrievedID = cat.getId();
         //then
-
-    }
-
-    @Test
-    public void test2CreateCat() {
-        //given
-
-        //when
-
-        //then
+        Assert.assertEquals(givenName, retrievedName);
+        Assert.assertEquals(givenBirthDate, retrievedDate);
+        Assert.assertTrue(retrievedID != null);
 
     }
 }
