126fa27a71f346f6cd9ff613def508e2e3fbea85
Nathan <natefintak@gmail.com>
2025-03-12 08:54:34 -0400

    CatHouse tests added

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,4 +1,5 @@
 package rocks.zipcodewilmington;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.Assert;
 import rocks.zipcodewilmington.animals.Cat;
@@ -17,30 +18,29 @@ public class CatHouseTest {
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
 
+    @BeforeEach
+    void setUp() {
+        CatHouse.clear();
+    }
+
+
     //void add(Cat cat)
     @Test
     public void test1addCat() {
-        //given
         String name = "Steph";
         Date birthDate = new Date(2017/5/13);
         Cat cat = AnimalFactory.createCat(name, birthDate);
-        CatHouse.clear();
-        //when
         CatHouse.add(cat);
-        //then
         CatHouse.getNumberOfCats();
     }
     //void remove(Integer id)
     @Test
     public void test1removeCatById() {
-        //given
         Cat cat1 = new Cat("Homer", new Date(1999/2/24), 0);
         Cat cat2 = new Cat("Achilles", new Date(2014/5/19), 1);
         CatHouse.add(cat1);
         CatHouse.add(cat2);
-        //when
         CatHouse.remove(1);
-        //then
         Assert.assertTrue(CatHouse.getCatById(0) != null);
         Assert.assertTrue(CatHouse.getCatById(1) == null);
         Assert.assertTrue(1 == CatHouse.getNumberOfCats());
@@ -48,15 +48,11 @@ public class CatHouseTest {
     //void remove(Cat cat)
     @Test
     public void test1removeCat() {
-        //given
         Cat cat1 = new Cat("Orpheus", new Date(2016/11/14), 0);
         Cat cat2 = new Cat("Eurydice", new Date(2017/10/29), 1);
         CatHouse.add(cat1);
         CatHouse.add(cat2);
-        //when
-        /// DON'T FORGET TO ADD THE CATS TO THE CATHOUSE BEFORE TRYING TO REMOVE/TEST FOR THEM
         CatHouse.remove(cat2);
-        //then
         Assert.assertTrue(CatHouse.getCatById(0) != null);
         Assert.assertTrue(CatHouse.getCatById(1) == null);
         Assert.assertTrue(1 == CatHouse.getNumberOfCats());
@@ -64,13 +60,28 @@ public class CatHouseTest {
     //return Cat for getCatById(Integer id)
     @Test
     public void test1getCatById() {
-        //given
-        Cat cat = new Cat("Tally", new Date(2022/2/18), 1);
-        CatHouse.add(cat);
-        //then
-        Assert.assertTrue(cat == CatHouse.getCatById(1));
+        Cat cat1 = new Cat("Tally", new Date(2022/2/18), 1);
+        CatHouse.add(cat1);
+        Assert.assertTrue(cat1 == CatHouse.getCatById(1));
+    }
+    //return Integer for getNumberOfCats()
+    @Test
+    public void test1getNumberOfCats() {
+        Cat cat1 = new Cat("Dina", new Date(2022/3/15), 4);
+        Cat cat2 = new Cat("Shelly", new Date(2023/6/12), 2);
+        Cat cat3 = new Cat("Riley", new Date(2021/11/19), 1);
+        CatHouse.add(cat1);
+        CatHouse.add(cat2);
+        CatHouse.add(cat3);
+        Assert.assertTrue(3 == CatHouse.getNumberOfCats());
     }
 
-    //return Integer for getNumberOfCats()
-
+    @Test
+    public void test2getNumberOfCats() {
+        Cat cat1 = new Cat("Dina", new Date(2022/3/15), 4);
+        Cat cat2 = new Cat("Shelly", new Date(2023/6/12), 2);
+        CatHouse.add(cat1);
+        CatHouse.add(cat2);
+        Assert.assertTrue(2 == CatHouse.getNumberOfCats());
+    }
 }
